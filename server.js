432ba9217220466d32c24c7b60afe31f66ecb7d1
Paulo Warren <paulowar@umich.edu>
2017-07-26 15:15:57 -0400

    add headers to server

--- server.js
+++ server.js
@@ -25,6 +25,11 @@ app.use(bp.urlencoded({extended: true}))
 
 app.use(session({secret: "secrat"}))
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://umich-dev.instructure.com');
+  next()
+})
+
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
   mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
